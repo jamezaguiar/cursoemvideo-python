8f2bf56b9b95d3cf00b7fe3f9516046e2cf1190f
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2019-12-09 23:17:29 -0300

    desafio 38 - mundo 2

---
+++ Mundo 2/038.py
@@ -0,0 +1,9 @@
+num1 = int(input("Primeiro número: "))
+num2 = int(input("Segundo número: "))
+
+if (num1 > num2):
+    print("O PRIMEIRO é maior")
+elif (num2 > num1):
+    print("O SEGUNDO é maior")
+else:
+    print("Os dois valores são IGUAIS")
